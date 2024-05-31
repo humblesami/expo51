dfda9b5d1c74426c97453c2bb2cf087d7a48f8e3
humblesami <samiakram@live.com>
2024-05-31 17:30:22 +0500

    trying to run with @react-native/metro-config

--- app/(tabs)/third.tsx
+++ app/(tabs)/third.tsx
@@ -1,10 +1,10 @@
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { StyleSheet, Image, Platform } from 'react-native';
 
-import { Collapsible } from '@/components/Collapsible';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { Collapsible } from '../../components/Collapsible';
+import ParallaxScrollView from '../../components/ParallaxScrollView';
+import { ThemedText } from '../../components/ThemedText';
+import { ThemedView } from '../../components/ThemedView';
 
 export default function TabThreeScreen() {
   return (
